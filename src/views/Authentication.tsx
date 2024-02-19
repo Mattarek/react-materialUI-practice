6461182854c8d3ffab5aad7e7e23f661634f9c23
Marcin Barc <marb637@gmail.com>
2024-02-19 19:22:21 +0100

    Styles for button added

--- src/views/Authentication.tsx
+++ src/views/Authentication.tsx
@@ -2,25 +2,39 @@ import Grid from '@mui/material/Grid';
 import { CommonButton } from '../components/common/commonButton/CommonButton';
 export const Authentication = () => {
     const buttonStyles = {
-        fontSize: 20,
-        fontWeight: 700,
-        backgroundColor: 'red',
-        '&:hover': {
-            backgroundColor: 'yellow',
+        fontWeight: 600,
+        fontSize: '0.875rem',
+        textTransform: 'capitalize',
+        borderRadius: 2.5,
+        '&.MuiButton-contained': {
+            backgroundColor: '#009db3',
+            '&:hover': {
+                backgroundColor: '#006db3',
+            },
+        },
+        '&.MuiButton-outlined': {
+            color: '#fff',
+            borderColor: '#fff',
+            '&:hover': {
+                backgroundColor: 'transparent',
+            },
         },
     };
 
     return (
         <Grid
             item
-            xs={8}>
+            xs={8}
+            sx={{ backgroundColor: '#009be5' }}>
             Authentication
             <CommonButton
-                size='large'
-                variant='contained'
-                color='error'
                 sx={buttonStyles}
-                disabled={false}>
+                variant='contained'>
+                Add user!
+            </CommonButton>
+            <CommonButton
+                variant='outlined'
+                sx={buttonStyles}>
                 Click!
             </CommonButton>
         </Grid>
