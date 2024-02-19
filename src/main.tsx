0b7888704bc07eb74d5aa0c8040543edb3654c5d
Marcin Barc <marb637@gmail.com>
2024-02-19 20:53:11 +0100

    theme added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { dashboardTheme } from './dashboardTheme.tsx';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Authentication } from './views/Authentication.tsx';
 import { Database } from './views/Database.tsx';
 import { ThemeProvider } from '@emotion/react';
-import { dashboardTheme } from './dashboardTheme.tsx';
 
 const router = createBrowserRouter([
     {
