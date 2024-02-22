aac51f469e74a2c27131fa6c744d559463752afa
Marcin Barc <marb637@gmail.com>
2024-02-22 17:07:56 +0100

    DataGrid added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from './components/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import { Grid } from '@mui/material';
+import { DataGridDemo } from './components/DataGrid/DataGrid';
 
 function App() {
     return (
         <Grid container>
             <Navbar />
             <Outlet />
+            <DataGridDemo />
         </Grid>
     );
 }
