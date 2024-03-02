9a19c79f45e5b75a538476a02f6d2417772954d1
Marcin Barc <marb637@gmail.com>
2024-03-02 20:52:54 +0100

    asqgqwgqdh5e35he34asaasvacascascsa5qdqdqdwqcascavasva

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,8 @@ const router = createBrowserRouter([
             { path: '/database', element: <Database /> },
             { path: '/storage', element: <Authentication /> },
             { path: '/hosting', element: <Authentication /> },
+            { path: '/functions', element: <Authentication /> },
+            { path: '/machinelLearning', element: <Authentication /> },
         ],
     },
     { path: '/dataGrid', element: <DataGridDemo /> },
