853a4210738b59332d40c55c1a37571b30ffd993
Marcin Barc <marb637@gmail.com>
2024-03-03 16:54:32 +0100

    ascascadxsxsxsascsacascsacacacascsvasvsavacacsac

--- src/main.tsx
+++ src/main.tsx
@@ -13,10 +13,6 @@ const router = createBrowserRouter([
         path: '',
         element: <App />,
         children: [
-            { path: '/authentication', element: <Authentication /> },
-            { path: '/database', element: <Database /> },
-            { path: '/storage', element: <Authentication /> },
-            { path: '/hosting', element: <Authentication /> },
             { path: '/functions', element: <Authentication /> },
             { path: '/machinelLearning', element: <Authentication /> },
         ],
